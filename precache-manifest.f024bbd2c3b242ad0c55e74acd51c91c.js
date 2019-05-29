b7cb50b8df7de7307e1c152f029641bb1cd9f1dc
thiru <thiru1921@gmail.com>
2019-05-29 23:17:45 +0530

    Updates

---
+++ precache-manifest.f024bbd2c3b242ad0c55e74acd51c91c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c03eb8ed9f204759e2063db9043666d",
+    "url": "/BankSearch/index.html"
+  },
+  {
+    "revision": "7ffc1195e55b2bd5fafb",
+    "url": "/BankSearch/static/css/2.c297138a.chunk.css"
+  },
+  {
+    "revision": "4f0eca04857df758f505",
+    "url": "/BankSearch/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7ffc1195e55b2bd5fafb",
+    "url": "/BankSearch/static/js/2.fc467a98.chunk.js"
+  },
+  {
+    "revision": "4f0eca04857df758f505",
+    "url": "/BankSearch/static/js/main.bb1e4121.chunk.js"
+  },
+  {
+    "revision": "8d478d6b4dc999ea175b",
+    "url": "/BankSearch/static/js/runtime~main.a80b00ea.js"
+  }
+]);
