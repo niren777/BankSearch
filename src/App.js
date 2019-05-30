5e2dd686a42de614e2aea3972888fd2cf45d6db9
thiru <thiru1921@gmail.com>
2019-05-30 22:44:53 +0530

    Integrate the redux into the app

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import './App.css';
 import Home from './components/Home';
 import Favourite from './components/Favourite';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import configureStore from "./modules/store";
+
+const reduxStore = configureStore(window.REDUX_INITIAL_DATA);
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Route exact path="/" component={Home} />
-        <Route path="/favourite" component={Favourite} />
-      </div>
-    </Router>
+    <ReduxProvider store={reduxStore}>
+      <Router basename="/BankSearch" >
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/favourite" component={Favourite} />
+        </div>
+      </Router>
+    </ReduxProvider>
   );
 }
 
